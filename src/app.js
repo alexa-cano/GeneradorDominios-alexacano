c95747f992bdb187e022a7d3fee9d65502ca0822
alexa-cano <115123267+alexa-cano@users.noreply.github.com>
2025-07-08 15:47:39 +0000

    generador de nombres de dominio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon', 'developer', 'nation'];
+  let extensions = ['.com', '.net', '.us', '.io', '.es'];
+
+  function domainHack(word, tld) {
+    if (word.endsWith(tld)) {
+      let base = word.slice(0, word.length - tld.length);
+      return base + '.' + tld;
+    }
+    return null;
+  }
+
+  // Seleccionamos el contenedor donde se mostrarán los dominios
+  const domainList = document.getElementById('domain-list');
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        let base = p + a + n;
+        for (let ext of extensions) {
+          // Dominio normal
+          let normalDomain = base + ext;
+          let normalItem = document.createElement('p');
+          normalItem.textContent = normalDomain;
+          domainList.appendChild(normalItem);
+
+          // Domain hack
+          let hacked = domainHack(base, ext.slice(1));
+          if (hacked) {
+            let hackItem = document.createElement('p');
+            hackItem.textContent = hacked;
+            domainList.appendChild(hackItem);
+          }
+        }
+      }
+    }
+  }
+
 };
